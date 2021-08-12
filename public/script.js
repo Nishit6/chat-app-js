cbec3d69224a9704e0f237af170ab2b27dfbd00a
Nishit singh <djnishu6@gmail.com>
2021-08-12 13:11:41 +0530

    second commit

--- public/script.js
+++ public/script.js
@@ -1,35 +1,79 @@
 const socket = io();
+const chat = document.querySelector('#chat'); 
+const login_btn = document.querySelector('#login-btn');
+const login_inp = document.querySelector('#login-inp');
+const login = document.querySelector('#login');
+const send = document.querySelector('#send');
+const text_area = document.querySelector('#text-area');
+
+const list = document.querySelector('#list');
 
 
-$('#chat').hide();
+chat.style.display ="none";
 
 
-
-$('#login-btn').click(()=>{
-
+login_btn.addEventListener("click",()=>{
     socket.emit('login',{
 
-        name:$('#login-inp').val()
+        name:login_inp.value
+       
     })
-        $('#login').hide();
-        $('#chat').show();
-
-
+    login.style.display ="none";
+    chat.style.display ="block";
+    
 })
 
-$('#send').click(()=>{
+send.addEventListener("click",()=>{
 
     socket.emit('send_msg',{
 
-        text:$('#text-area').val()
+        text:text_area.value
 
     })
-    $('#text-area').val("")
+    text_area.value = ""
+    
+    $('.emojionearea-editor').text("");
+    
 })
 
 socket.on('recieved_msg',(data)=>{
 
-    $('#list').append(`<li> <strong>${data.name}</strong> :  ${data.msg}</li>`)
-
-
+    const li= document.createElement("li");
+    li.innerHTML = (` <strong>${data.name}</strong> :  ${data.msg}`)
+    document.body.append(li);
+   
 })
+
+
+// $('#login-btn').click(()=>{
+
+//     socket.emit('login',{
+
+//         name:$('#login-inp').val()
+//     })
+//         $('#login').hide();
+//         $('#chat').show();
+
+
+// })
+
+// $('#send').click(()=>{
+
+//     socket.emit('send_msg',{
+
+//         text:$('#text-area').val()
+
+//     })
+//     $('#text-area').val("")
+//     $('.emojionearea-editor').text("");
+// })
+
+// socket.on('recieved_msg',(data)=>{
+
+//     $('#list').prepend(`<li> <strong>${data.name}</strong> :  ${data.msg}</li>`)
+
+   
+// })
+
+
+
